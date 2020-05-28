0a8b6b465d97a91bde23337d07ebbedfb46c1892
rabah babaci <4rabah@gmail.com>
2020-05-28 16:26:20 -0700

    define the recipes router

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const recipesRouter = require("./api/recipesRouter");
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use("/api/recipes", recipesRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API is up!!!" });
