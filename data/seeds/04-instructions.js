d0ad83c95bf52c4383d4e7d03c93828e8ec8fd0c
rabah babaci <4rabah@gmail.com>
2020-05-29 16:28:59 -0700

    update the instructions seed

--- data/seeds/04-instructions.js
+++ data/seeds/04-instructions.js
@@ -3,11 +3,11 @@ exports.seed = function (knex) {
     .del()
     .then(function () {
       return knex("instructions").insert([
-        { instruction: "Wash your hands!" },
-        { instruction: "Enjoy your meal!" },
-        { instruction: "Turn oven on" },
-        { instruction: "Turn back off" },
-        { instruction: "Figure it out" },
+        { recipe_id: 1, step_number: 2, instruction: "Wash your hands!" },
+        { recipe_id: 2, step_number: 1, instruction: "Enjoy your meal!" },
+        { recipe_id: 2, step_number: 3, instruction: "Turn oven on" },
+        { recipe_id: 2, step_number: 2, instruction: "Turn back off" },
+        { recipe_id: 4, step_number: 2, instruction: "Figure it out" },
       ]);
     });
 };
