ecf1396238b491ad689f00a361177f78736bacbe
rabah babaci <4rabah@gmail.com>
2020-05-29 09:59:05 -0700

    implement the get recipes endpoint

--- api/dbHelpers.js
+++ api/dbHelpers.js
@@ -1,3 +1,5 @@
 const db = require("../data/dbConfig");
 
 const getRecipes = () => db("recipes");
+
+module.exports = { getRecipes };
--- api/recipesRouter.js
+++ api/recipesRouter.js
@@ -1,5 +1,17 @@
 const express = require("express");
 
+const { getRecipes } = require("./dbHelpers");
+
 const router = express.Router();
+
+router.get("/", async (res, req, next) => {
+  try {
+    const recipes = await getRecipes();
+
+    res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
