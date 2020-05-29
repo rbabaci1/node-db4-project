bd3be6377fb373c8a3ed704eb4426e59025e1601
rabah babaci <4rabah@gmail.com>
2020-05-29 10:18:28 -0700

    create the getShoppingList database helper

--- api/dbHelpers.js
+++ api/dbHelpers.js
@@ -2,4 +2,11 @@ const db = require("../data/dbConfig");
 
 const getRecipes = () => db("recipes");
 
-module.exports = { getRecipes };
+const getShoppingList = recipe_id => {
+  return db("recipes as r")
+    .select("recipes.*", "ri.ingredient_id", "ri.ingredient_quantity")
+    .join("recipe_ingredient as ri", "r.id", "ri.recipe_id")
+    .where({ recipe_id });
+};
+
+module.exports = { getRecipes, getShoppingList };
