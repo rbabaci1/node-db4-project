7548878a403cd07c1b201d1d3bb5f734bb454f62
rabah babaci <4rabah@gmail.com>
2020-05-28 18:03:58 -0700

    remove all recipes ids and let database generate them

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -4,37 +4,31 @@ exports.seed = function (knex) {
     .then(function () {
       return knex("recipes").insert([
         {
-          id: 1,
           name: "CousCous",
           cuisine_type: "Algerian",
           creator: "Rabah Babaci",
         },
         {
-          id: 1,
           name: "Pizza",
           cuisine_type: "Italian",
           creator: "Kyla Gifford",
         },
         {
-          id: 1,
           name: "Omelette",
           cuisine_type: "American",
           creator: "Mike Tyson",
         },
         {
-          id: 1,
           name: "Tacos",
           cuisine_type: "Mexican",
           creator: "Me. Yessss!",
         },
         {
-          id: 1,
           name: "Lentil Soup",
           cuisine_type: "French",
           creator: "Itself",
         },
         {
-          id: 1,
           name: "New York Steak",
           cuisine_type: "American",
           creator: "Ramsey",
