aae56144721ec89996263ebc803a36cb163e5343
rabah babaci <4rabah@gmail.com>
2020-05-29 11:41:14 -0700

    test getInstructions db handler

--- data/seeds/05-recipe-instructions.js
+++ data/seeds/05-recipe-instructions.js
@@ -4,10 +4,10 @@ exports.seed = function (knex) {
     .then(function () {
       return knex("recipe_instructions").insert([
         { recipe_id: 3, instruction_id: 1, step_number: 6 },
-        { recipe_id: 3, instruction_id: 1, step_number: 1 },
-        { recipe_id: 3, instruction_id: 1, step_number: 4 },
+        { recipe_id: 3, instruction_id: 3, step_number: 4 },
+        { recipe_id: 3, instruction_id: 2, step_number: 2 },
         { recipe_id: 2, instruction_id: 1, step_number: 10 },
-        { recipe_id: 3, instruction_id: 5, step_number: 2 },
+        { recipe_id: 3, instruction_id: 5, step_number: 3 },
       ]);
     });
 };
